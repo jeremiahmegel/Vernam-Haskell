512fb8a6dd6c3f9cac0de32f549a0aae0db5e406
Jeremiah Megel <github@jeremiahm.com>
2015-11-15 15:36:48 -0600

    Prettier code

--- vernam.hs
+++ vernam.hs
@@ -1,13 +1,13 @@
-import Data.Char (ord, chr)
 import Data.Bits (xor)
-import System.IO (getContents, readFile, IOMode(ReadMode))
-import System.Environment (getArgs)
-import Data.Maybe (isJust, isNothing, fromJust, fromMaybe)
-import Data.List (elemIndex, intersect, intercalate, deleteFirstsBy)
+import Data.Char (chr, ord)
 import Data.Function (on)
+import Data.List (deleteFirstsBy, elemIndex, intercalate, intersect)
+import Data.Maybe (fromJust, fromMaybe, isJust, isNothing)
+import System.Environment (getArgs)
+import System.IO (getContents, readFile)
 
 vernam :: String -> String -> String
-vernam message key = foldl (\acc char -> acc ++ [chr (xor (ord char) (ord (key !! (length acc `mod` length key))))]) [] message
+vernam message key = foldl (\acc char -> acc ++ [chr $ xor (ord char) $ ord $ key !! (length acc `mod` length key)]) [] message
 
 argList :: [String] -> [(Maybe String, Maybe String)]
 argList = foldl argEval []
@@ -20,17 +20,17 @@ argList = foldl argEval []
 		else if null args || isJust (snd $ last args) then
 			args ++ [(Nothing, Just arg)]
 		else
-			init args ++ [((fst $ last args), Just arg)]
+			init args ++ [(fst $ last args, Just arg)]
 
-argChoose :: [(String, ((Maybe String) -> a))] -> [(Maybe String, Maybe String)] -> Maybe a
+argChoose :: [(String, (Maybe String -> a))] -> [(Maybe String, Maybe String)] -> Maybe a
 argChoose choices args =
 	if length valid == 1 then
-		Just ((fromJust $ lookup (fromJust $ head valid) choices) (fromJust $ lookup (Just (fromJust $ head valid)) args))
+		Just $ (fromJust $ lookup (fromJust $ head valid) choices) $ fromJust $ lookup (Just $ fromJust $ head valid) args
 	else if length valid == 0 then
 		Nothing
 	else
 		error $ "Argument conflict: " ++ (intercalate ", " $ map fromJust valid)
-	where valid = intersect (map (Just . fst) choices) (map fst args)
+	where valid = intersect (map (Just . fst) choices) $ map fst args
 
 assertArgVal :: String -> (String -> a) -> Maybe String -> a
 assertArgVal arg f s =
@@ -48,12 +48,12 @@ invalidFlags valid args = deleteFirstsBy ((==) `on` fst) args $ map (\x -> (x, N
 main = do
 	args <- getArgs
 	let badArgs = invalidFlags (map Just ["-i", "-if", "-k", "-kf"]) $ argList args
-	if not $ null $ badArgs then
+	if not $ null badArgs then
 		error $ "Invalid arguments: " ++ (show $ map (\(a, b) -> (fromMaybe "" a, fromMaybe "" b)) $ badArgs)
 	else
 		let
-			inputArg = argChoose [argVal "-i" return, argVal "-if" (\filename -> readFile filename)] $ argList args
-			keyArg = argChoose [argVal "-k" return, argVal "-kf" (\filename -> readFile filename)] $ argList args
+			inputArg = argChoose [argVal "-i" return, argVal "-if" readFile] $ argList args
+			keyArg = argChoose [argVal "-k" return, argVal "-kf" readFile] $ argList args
 		in
 			if isJust keyArg then do
 				input <-
@@ -64,5 +64,5 @@ main = do
 				key <- fromJust keyArg
 				putStr $ vernam input key
 			else
-				error "Invalid arguments."
+				error "Missing argument(s)"
 
