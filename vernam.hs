f11bfd5589efed08aeade9038bebb226cf9061f3
Jeremiah Megel <github@jeremiahm.com>
2015-11-15 14:55:00 -0600

    Revamped argument-parser

--- vernam.hs
+++ vernam.hs
@@ -2,63 +2,67 @@ import Data.Char (ord, chr)
 import Data.Bits (xor)
 import System.IO (getContents, readFile, IOMode(ReadMode))
 import System.Environment (getArgs)
-import Data.Maybe (isJust, fromJust)
-import Data.List (elemIndex, intersect)
+import Data.Maybe (isJust, isNothing, fromJust, fromMaybe)
+import Data.List (elemIndex, intersect, intercalate, deleteFirstsBy)
+import Data.Function (on)
 
 vernam :: String -> String -> String
 vernam message key = foldl (\acc char -> acc ++ [chr (xor (ord char) (ord (key !! (length acc `mod` length key))))]) [] message
 
-argVal :: String -> [String] -> Maybe String
-argVal arg args =
-	if isJust indMaybe then
-		let ind = fromJust indMaybe in
-			if (ind + 2) <= length args && not (head (args !! (ind + 1)) == '-') then
-				Just (args !! (ind + 1))
+argList :: [String] -> [(Maybe String, Maybe String)]
+argList = foldl argEval []
+	where argEval args arg =
+		if (head arg) == '-' then
+			if isNothing $ lookup (Just arg) args then
+				args ++ [(Just arg, Nothing)]
 			else
-				Nothing
-	else
-		Nothing
-	where
-		indMaybe = arg `elemIndex` args
+				error $ "Duplicate argument: " ++ arg
+		else if null args || isJust (snd $ last args) then
+			args ++ [(Nothing, Just arg)]
+		else
+			init args ++ [((fst $ last args), Just arg)]
 
-argChoose :: [(String, a)] -> [String] -> Maybe (String, a)
-argChoose choices args =	
+argChoose :: [(String, ((Maybe String) -> a))] -> [(Maybe String, Maybe String)] -> Maybe a
+argChoose choices args =
 	if length valid == 1 then
-		Just ((head valid), fromJust (lookup (head valid) choices))
+		Just ((fromJust $ lookup (fromJust $ head valid) choices) (fromJust $ lookup (Just (fromJust $ head valid)) args))
 	else if length valid == 0 then
 		Nothing
 	else
-		error $ "Argument conflict: " ++ (show valid)
-	where valid = intersect (map fst choices) args
+		error $ "Argument conflict: " ++ (intercalate ", " $ map fromJust valid)
+	where valid = intersect (map (Just . fst) choices) (map fst args)
 
-argValChoose :: [(String, (String -> a))] -> [String] -> Maybe a
-argValChoose choices args =
-	if isJust argMaybe then
-		let
-			arg = fromJust argMaybe
-			val = argVal (fst arg) args
-		in
-			if isJust val then
-				Just ((snd arg) (fromJust val))
-			else
-				error $ "No value provided: " ++ fst arg
+assertArgVal :: String -> (String -> a) -> Maybe String -> a
+assertArgVal arg f s =
+	if isJust s then
+		f $ fromJust s
 	else
-		Nothing
-	where
-		argMaybe = argChoose choices args
+		error $ "No value provided: " ++ arg
+
+argVal :: String -> (String -> a) -> (String, (Maybe String) -> a)
+argVal arg f = (arg, assertArgVal arg f)
+
+invalidFlags :: [Maybe String] -> [(Maybe String, Maybe String)] -> [(Maybe String, Maybe String)]
+invalidFlags valid args = deleteFirstsBy ((==) `on` fst) args $ map (\x -> (x, Nothing)) valid
 
 main = do
 	args <- getArgs
-	let
-		inputArg = argValChoose [("-i", return), ("-if", (\filename -> readFile filename))] args
-		keyArg = argValChoose [("-k", return), ("-kf", (\filename -> readFile filename))] args
-	if isJust keyArg then do
-		input <- if isJust inputArg then
-			fromJust inputArg
-		else
-			getContents
-		key <- fromJust keyArg
-		putStr $ vernam input key
+	let badArgs = invalidFlags (map Just ["-i", "-if", "-k", "-kf"]) $ argList args
+	if not $ null $ badArgs then
+		error $ "Invalid arguments: " ++ (show $ map (\(a, b) -> (fromMaybe "" a, fromMaybe "" b)) $ badArgs)
 	else
-		error "Invalid arguments."
+		let
+			inputArg = argChoose [argVal "-i" return, argVal "-if" (\filename -> readFile filename)] $ argList args
+			keyArg = argChoose [argVal "-k" return, argVal "-kf" (\filename -> readFile filename)] $ argList args
+		in
+			if isJust keyArg then do
+				input <-
+					if isJust inputArg then
+						fromJust inputArg
+					else
+						getContents
+				key <- fromJust keyArg
+				putStr $ vernam input key
+			else
+				error "Invalid arguments."
 
