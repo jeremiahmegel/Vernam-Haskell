c5f5c4783a29332a322eb98f77b844c7bf15ae5d
Jeremiah Megel <github@jeremiahm.com>
2016-01-03 23:17:32 -0600

    Updated to work with ByteStrings instead of Strings

--- vernam.hs
+++ vernam.hs
@@ -3,66 +3,74 @@ import Data.Char (chr, ord)
 import Data.Function (on)
 import Data.List (deleteFirstsBy, elemIndex, intercalate, intersect)
 import Data.Maybe (fromJust, fromMaybe, isJust, isNothing)
-import System.Environment (getArgs)
-import System.IO (getContents, readFile)
+import System.Posix.Env.ByteString (getArgs)
+import qualified System.Environment as E
 
-vernam :: String -> String -> String
-vernam = zipWith (\k m -> chr $ (xor `on` ord) k m) . cycle
+import qualified Data.ByteString as B
+import Data.Word (Word8)
+import Data.String (IsString, fromString)
 
-argList :: [String] -> [(Maybe String, Maybe String)]
+type Arg = (String, B.ByteString)
+
+vernam :: B.ByteString -> B.ByteString -> B.ByteString
+vernam key message = B.pack $ zipWith xor (cycle $ B.unpack key) (B.unpack message)
+
+argList :: [Arg] -> [(Maybe Arg, Maybe Arg)]
 argList = foldl argEval []
 	where argEval args arg =
-		if head arg == '-' then
+		if (head . fst) arg == '-' then
 			if isNothing $ lookup (Just arg) args then
 				args ++ [(Just arg, Nothing)]
 			else
-				error $ "Duplicate argument: " ++ arg
+				error "Duplicate argument"
 		else if null args || isJust (snd $ last args) then
 			args ++ [(Nothing, Just arg)]
 		else
 			init args ++ [(fst $ last args, Just arg)]
 
-argChoose :: [(String, (Maybe String -> a))] -> [(Maybe String, Maybe String)] -> Maybe a
+argChoose :: [(String, (Maybe Arg -> a))] -> [(Maybe Arg, Maybe Arg)] -> Maybe a
 argChoose choices args =
 	if length valid == 1 then
-		Just $ (fromJust $ lookup (fromJust $ head valid) choices) $ fromJust $ lookup (Just $ fromJust $ head valid) args
+		Just $ (fromJust $ lookup (fromJust $ head valid) choices) $ fromJust $ lookup (Just $ fromJust $ head valid) (map (\(x, y) -> (fmap fst x, y)) args)
 	else if length valid == 0 then
 		Nothing
 	else
-		error $ "Argument conflict: " ++ (intercalate ", " $ map fromJust valid)
-	where valid = intersect (map (Just . fst) choices) $ map fst args
+		error "Argument conflict"
+	where valid = intersect (map (Just . fst) choices) $ map (fmap fst . fst) args
 
-assertArgVal :: String -> (String -> a) -> Maybe String -> a
+assertArgVal :: String -> (Arg -> a) -> Maybe Arg -> a
 assertArgVal arg f s =
 	if isJust s then
 		f $ fromJust s
 	else
-		error $ "No value provided: " ++ arg
+		error "No value provided for argument"
 
-argVal :: String -> (String -> a) -> (String, (Maybe String) -> a)
+argVal :: String -> (Arg -> a) -> (String, (Maybe Arg) -> a)
 argVal arg f = (arg, assertArgVal arg f)
 
-invalidFlags :: [Maybe String] -> [(Maybe String, Maybe String)] -> [(Maybe String, Maybe String)]
-invalidFlags valid args = deleteFirstsBy ((==) `on` fst) args $ map (\x -> (x, Nothing)) valid
+invalidFlags :: [Maybe String] -> [(Maybe Arg, Maybe Arg)] -> [(Maybe String, Maybe String)]
+invalidFlags valid args = deleteFirstsBy ((==) `on` fst) (map (\(x, y) -> (fmap fst x, fmap fst y)) args) $ map (\x -> (x, Nothing)) valid
 
 main = do
-	args <- getArgs
+	cleanArgs <- E.getArgs
+	bsArgs <- getArgs
+	let args = zip cleanArgs bsArgs
 	let badArgs = invalidFlags (map Just ["-i", "-if", "-k", "-kf"]) $ argList args
 	if not $ null badArgs then
-		error $ "Invalid arguments: " ++ (show $ map (\(a, b) -> (fromMaybe "" a, fromMaybe "" b)) $ badArgs)
+		error "Invalid arguments"
 	else
 		let
-			inputArg = argChoose [argVal "-i" return, argVal "-if" readFile] $ argList args
-			keyArg = argChoose [argVal "-k" return, argVal "-kf" readFile] $ argList args
+			inputArg = argChoose [argVal "-i" (return . snd), argVal "-if" (B.readFile . fst)] $ argList args
+			keyArg = argChoose [argVal "-k" (return . snd), argVal "-kf" (B.readFile . fst)] $ argList args
 		in
 			if isJust keyArg then do
 				input <-
 					if isJust inputArg then
 						fromJust inputArg
 					else
-						getContents
+						B.getContents
 				key <- fromJust keyArg
-				putStr $ vernam key input
+				B.putStr $ vernam key input
 			else
 				error "Missing argument(s)"
 
