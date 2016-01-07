2661dc524545a74e8a659563591e8010d2f561b6
Jeremiah Megel <github@jeremiahm.com>
2016-01-06 20:27:02 -0600

    Explicitly declared Main module

--- Main.hs
+++ Main.hs
@@ -15,6 +15,8 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
+module Main (main) where
+
 import Control.Applicative ((<*>))
 import Data.Bits (xor)
 import qualified Data.ByteString as B
